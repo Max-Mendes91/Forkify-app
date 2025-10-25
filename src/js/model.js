c124a3537887365f23778b68b25aa75b576670a3
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-10-25 12:14:50 +0200

    Refactor loadRecipe function to utilize getJSON for API calls, improving error handling and code clarity.

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,14 @@
+import { async } from 'regenerator-runtime';
+import {API_URL} from './config.js';
+import { getJSON } from './helpers.js'; 
 export const state = {
     recipe: {}
 };
 
 export const loadRecipe = async function name(id) {
     try {
-        const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
-        const data = await res.json();
 
-        if (!res.ok) throw new Error(`${data.message}(${res.status})`);
-
-
-        console.log(data);
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const { recipe } = data.data;
         state.recipe = {
@@ -25,7 +23,9 @@ export const loadRecipe = async function name(id) {
         }
         console.log(state.recipe);
     } catch (err) {
-        alert(err);
+        //temporary error handling 
+        console.error(`${err} 💥💥💥💥`);
+       
 
     };
 }
